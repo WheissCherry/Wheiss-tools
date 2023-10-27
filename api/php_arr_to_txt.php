efe8a038e9d66e2bb569699061830a0f62a49130
wheiss <94127216+qingying2022@users.noreply.github.com>
2023-10-28 01:27:22 +0800

    Update php_arr_to_txt.php

--- api/php_arr_to_txt.php
+++ api/php_arr_to_txt.php
@@ -3,9 +3,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$user_path = $_POST["user_path"];
 	$arr_data = $_POST["arr_data"];
 	if (empty($user_path)) {
-		die("Plase input your php file address such as".PHP_EOL."https://www.123.xyz/1.php?id=");
+		die("Please input your php file address such as".PHP_EOL."https://www.123.xyz/1.php?id=");
 	} else if (empty($arr_data)) {
-		die("Plase input your php array such as".PHP_EOL."'cctv1' => 'CCTV1SD_1200', //CCTV1");
+		die("Please input your php array such as".PHP_EOL."'cctv1' => 'CCTV1SD_1200', //CCTV1");
 	}
 	$pattern = "/.*['\"](\w+)['\"].*=.+\/\/(.+)/";
 	if(preg_match_all($pattern,$arr_data,$matches)){
