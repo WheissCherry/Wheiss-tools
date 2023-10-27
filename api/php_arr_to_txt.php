ab4a4d8d89ea4c614a101806335d4f68d6cc186f
wheiss <94127216+qingying2022@users.noreply.github.com>
2023-10-27 22:01:54 +0800

    Add files via upload

---
+++ api/php_arr_to_txt.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$user_path = $_POST["user_path"];
+	$arr_data = $_POST["arr_data"];
+	if (empty($user_path)) {
+		die("Plase input you php file address such as".PHP_EOL."https://www.123.xyz/1.php?id=");
+	} else if (empty($arr_data)) {
+		die("Plase input you php array such as".PHP_EOL."'cctv1' => 'CCTV1SD_1200', //CCTV1");
+	}
+$pattern = "/.*['\"](\w+)['\"].*=.+\/\/(.+)/";
+	if(preg_match_all($pattern,$arr_data,$matches)){
+		for($i=0;$i<count($matches[1]);$i++){
+        echo $matches[2][$i].','.$user_path.$matches[1][$i].PHP_EOL;
+		}
+	}
+}
+?>
