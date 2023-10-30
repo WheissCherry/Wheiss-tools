4c7dab62c57d5b3ec988b02ad32121b9a2db9612
wheiss <94127216+qingying2022@users.noreply.github.com>
2023-10-30 19:11:55 +0800

    Update php_arr_to_txt.php

--- api/php_arr_to_txt.php
+++ api/php_arr_to_txt.php
@@ -1,15 +1,20 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	$user_path = $_POST["user_path"];
-	$arr_data = $_POST["arr_data"];
-	if (empty($user_path)) {
-		die("Please input your php file address such as".PHP_EOL."https://www.123.xyz/1.php?id=");
+	$user_path = $_POST["user_path"]??'';
+	$arr_data = $_POST["arr_data"]??'';
+	$scheme = $_POST["scheme"]??'';
+	if (strlen($user_path)>80) {
+		die("Address length is exceeded!");
 	} else if (empty($arr_data)) {
-		die("Please input your php array such as".PHP_EOL."'cctv1' => 'CCTV1SD_1200', //CCTV1");
+		die("Please input your array data.");
 	}
-	$pattern = "/.*['\"](\w+)['\"].*=.+\/\/(.+)/";
-	if(preg_match_all($pattern,$arr_data,$matches)){
-		for($i=0;$i<count($matches[1]);$i++){
+	if ($scheme == ''){
+		$pattern = "/.*['\"](\w+)['\"].+>.+\/\/(.*)/";
+	} else {
+		$pattern = "/.+>.*['\"](\S+)['\"].*\/\/(.*)/";
+	}
+	if (preg_match_all($pattern,$arr_data,$matches)){
+		for($i = 0; $i < count($matches[1]); $i++){
 			echo $matches[2][$i].','.$user_path.$matches[1][$i].PHP_EOL;
 		}
 	}
